03832764a8e337b55bb3dacec14a3beab575838f
Elias Khan <khan.elias@gmail.com>
2020-12-02 19:24:44 +0100

    done!

--- src/main/java/com/realdolmen/days/day2/Day2.java
+++ src/main/java/com/realdolmen/days/day2/Day2.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.stream.Stream;
 
 public class Day2 {
 
@@ -13,40 +12,32 @@ public class Day2 {
 
     public static void main(String[] args) {
 
-        try (Stream<String> lines = new BufferedReader(
+        try (var lines = new BufferedReader(
                 new FileReader(
-                        new File("src/main/resources/input-day-2")
-                )
-        ).lines()) {
+                        new File("src/main/resources/input-day-2")))
+                .lines()) {
 
             lines.forEach(s -> {
                 // input prep
-                String[] splitOverColon = s.split(":");
-                String[] splitRules = splitOverColon[0].split("[- ]");
-                int min = Integer.parseInt(splitRules[0]);
-                int max = Integer.parseInt(splitRules[1]);
-                char match = splitRules[2].charAt(0);
-                char[] password = splitOverColon[1].toCharArray();
-                int charCount = 0;
+                var splitOverColon = s.split(":");
+                var splitRules = splitOverColon[0].split("[- ]");
+                var min = Integer.parseInt(splitRules[0]);
+                var max = Integer.parseInt(splitRules[1]);
+                var match = splitRules[2].charAt(0);
+                var password = splitOverColon[1].strip().toCharArray();
 
                 // first part
+                var charCount = 0;
                 for (char c : password) {
                     if (c == match) charCount++;
                 }
-                if (charCount >= min && charCount <= max) {
-                    result1++;
-                }
+                if (charCount >= min && charCount <= max) result1++;
 
                 // second part
-                if (password[min - 1] == match ^ password[max - 1] == match) {
-                    result2++;
-                }
+                if (password[min - 1] == match ^ password[max - 1] == match) result2++;
             });
-
             System.out.println(result1);
             System.out.println(result2);
-
-
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
