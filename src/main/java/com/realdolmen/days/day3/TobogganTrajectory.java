a782810368da774b63e6c9626c1c8d5acbe40f91
Elias Khan <khan.elias@gmail.com>
2020-12-03 19:59:18 +0100

    day 3 part 2 (reduce operation to multiply all results returns wrong answer :'( )

--- src/main/java/com/realdolmen/days/day3/TobogganTrajectory.java
+++ src/main/java/com/realdolmen/days/day3/TobogganTrajectory.java
@@ -1,5 +1,8 @@
 package com.realdolmen.days.day3;
 
+import com.google.common.collect.Multimap;
+import com.google.common.collect.MultimapBuilder;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -26,22 +29,52 @@ public class TobogganTrajectory {
 				rows.add(list);
 			});
 			
-			// y keeps track of y-axis position
-			int y = 0;
-			int counter = 0;
+			// part 1
+			bombTheHill(rows, 3, 1);
 			
-			// traverse rows and check for a 1 right after continuing from the last row
-			for (var row : rows) {
-				if (row.get(y) == 1) counter++;
-				y = y + 3;
-				// if y passes the right bound subtract the row size from y to wrap around to beginning
-				if (y >= row.size()) y = y - row.size();
-			}
+			// part 2
+			// Guava Multimap for possible routes because first and last entry have duplicate keys
+			// And Java has no Collection/Map that supports duplicate keys
+			// With a classic Map could've used the elegant Map.ofEntries...
+			Multimap<Integer, Integer> map = MultimapBuilder.hashKeys().arrayListValues().build();
+			map.put(1, 1);
+			map.put(3, 1);
+			map.put(5, 1);
+			map.put(7, 1);
+			map.put(1, 2);
 			
-			System.out.println(counter);
+			int result = map
+					.entries()
+					.stream()
+					.mapToInt(entry -> bombTheHill(rows, entry.getKey(), entry.getValue()))
+					.reduce(1, (a, b) -> a * b);
+			
+			System.out.printf("The answer to part 2 is: %d", result);
 			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private static int bombTheHill(ArrayList<ArrayList<Byte>> rows, int slope, int speed) {
+		// y keeps track of y-axis position
+		int y = 0;
+		int counter = 0;
+		
+		// iterate rows and check for a 1 right after descending {speed} number of rows
+		for (int i = 0; i < rows.size(); i = i + speed) {
+			ArrayList<Byte> row = rows.get(i);
+			
+			// check for a tree
+			if (row.get(y) == 1) counter++;
+			y = y + slope;
+			
+			// if y passes the right bound, subtract the row size from y to wrap around to beginning
+			if (y >= row.size()) y = y - row.size();
+		}
+		
+		System.out.printf("For slope %d and speed %d you encounter %d trees\n", slope, speed, counter);
+		
+		return counter;
 	}
 }
