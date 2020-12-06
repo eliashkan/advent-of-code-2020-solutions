cbb06715cc7cee4c9beb6b0d4b9ce27e390809b4
Elias Khan <khan.elias@gmail.com>
2020-12-06 21:27:09 +0100

    day 5 part 2

--- src/main/java/com/realdolmen/days/day5/BinaryBoarding.java
+++ src/main/java/com/realdolmen/days/day5/BinaryBoarding.java
@@ -2,7 +2,9 @@ package com.realdolmen.days.day5;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Optional;
+import java.util.stream.IntStream;
 
 import static java.nio.file.Files.readAllLines;
 import static java.util.Comparator.comparingInt;
@@ -11,20 +13,37 @@ public class BinaryBoarding {
 	
 	public static void main(String[] args) {
 		
+		ArrayList<Integer> ids = new ArrayList<>();
 		try {
+			// Part 1
 			Optional<Integer> max = readAllLines(Paths.get("src/main/resources/input-day-5"))
 					.stream()
 					.map(line -> new String[]{line.substring(0, 7), line.substring(7)})
 					.map(array -> {
 						int row = binarySearch(array[0], 0, 127);
 						int column = binarySearch(array[1], 0, 7);
-						return row * 8 + column;
+						int id = row * 8 + column;
+						ids.add(id);
+						return id;
 					})
 					.max(comparingInt(o -> o));
 			System.out.println(max.orElse(-1));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		// Part 2
+		// Find sum of all possible id's
+		// Need to find min and max in actual id's because there's seats missing at beginning and end
+		// Difference between sum of all possible id's and sum of all actual id's in our input = our missing seat id.
+		int min = ids.stream().min(Integer::compareTo).get();
+		int max = ids.stream().max(Integer::compareTo).get();
+		int expected = IntStream.range(min, max + 1).sum();
+		int actual = ids.stream().mapToInt(i -> i).sum();
+		int missingId = expected - actual;
+		
+		System.out.println("result: " + missingId);
+		
 	}
 	
 	public static int binarySearch(String sequence, int low, int high) {
